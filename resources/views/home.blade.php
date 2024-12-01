fe54df120e4afb0048a278e589bf8304ebbb8b55
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-12-01 11:47:56 +0600

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,27 @@
     <div class="content-wrapper">
 
 
-        <breadcrumb-component :title="'Dashboard'"
-            :items="{{ json_encode(['home', 'dashboard']) }}"></breadcrumb-component>
+
+
+
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Dashboard</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item">
+                                <a href="{{ route('home') }}" class="text-capitalize">home</a>
+                                <span class="text-gray"> / </span>
+                                <span class="text-gray">Dashboard</span>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <section class="content">
             <div class="container-fluid">
