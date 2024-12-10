bc9ec120aa498b5a668bc44085ea5f791b9d6068
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-12-10 16:27:40 +0600

    category

--- resources/views/category/list.blade.php
+++ resources/views/category/list.blade.php
@@ -2,10 +2,7 @@
 
 @section('content')
     <div class="content-wrapper overflow-x-hidden">
-
-
         <breadcrumb-component :title="'Category List'" :items="{{ json_encode(['home', 'category', 'Category List']) }}"></breadcrumb-component>
         <category-list-component></category-list-component>
-
     </div>
 @endsection
