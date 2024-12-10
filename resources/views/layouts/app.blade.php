8c90f51403da410d60a59615eae455af53bd36be
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-12-10 10:41:09 +0600

    show sales detains done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,8 @@
 
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.1/css/responsive.dataTables.min.css">
+
 
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -137,6 +139,8 @@
     <script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+
 
 
     <script>
--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -41,27 +41,79 @@
                     </div>
 
                     <div class="card-body">
-                        <table class="table table-bordered display table-hover" id="salesList">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Code</th>
-                                    <th scope="col">Invoice Date</th>
-                                    <th scope="col">Customer Name</th>
-                                    <th scope="col">Total Amount</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody></tbody>
-                        </table>
+                        <div class="table-responsive">
+                            <table class="table table-bordered display table-hover nowrap" id="salesList"
+                                style="width: 100%">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Code</th>
+                                        <th>Invoice Date</th>
+                                        <th>Customer Name</th>
+                                        <th>Total Amount</th>
+                                        <th>Status</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody></tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-
         </section>
-
-
+        <div class="modal fade" id="showSalesDetails" tabindex="-1" aria-labelledby="showSalesDetailsLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header" style="height: 50px">
+                        <h5 class="modal-title" id="showSalesDetailsLabel">
+                            Sales Details
+                            <a class="btn btn-sm btn-outline-primary salesEditLink" href="#">
+                                <i class="fa-solid fa-pen" aria-hidden="true"></i>
+                                Edit
+                            </a>
+                        </h5>
+                        <button type="button" class="btn-close text-white" data-bs-dismiss="modal"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <h5><b>Sales Info:</b></h5>
+                            <p class="col-md-4 col-sm-2"><b>Code:</b> <span id="salesCode"></span></p>
+                            <p class="col-md-4 col-sm-2"><b>Invoice Date:</b> <span id="invoiceDate"></span></p>
+                            <p class="col-md-4 col-sm-2"><b>Total Amount:</b> <span id="totalAmount"></span></p>
+                            <p class="col-md-4 col-sm-2"><b>Paid Amount:</b> <span id="paidAmount"></span></p>
+                            <p class="col-md-4 col-sm-2"><b>Due Amount:</b> <span id="dueAmount"></span></p>
+                            <p class="col-md-4 col-sm-2"><b>Note:</b> <span id="salesNote"></span></p>
+                            <hr />
+                            <h5><b>Sales Details:</b></h5>
+                            <table class="table px-2">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">Particulars</th>
+                                        <th scope="col" style="width: 7rem">Qty</th>
+                                        <th scope="col">Sales</th>
+                                        <th scope="col" style="width: 7rem">Discount</th>
+                                        <th scope="col">Total</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="salesDetailsBody"></tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td colspan="5" class="text-end"><b>Total :</b></td>
+                                        <td colspan="2" class="text-start"><span id="finalTotal"></span> tk</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary text-white" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 @endsection
@@ -126,17 +178,17 @@
                     }, {
                         render: function(data, type, row) {
                             const btns = `
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-success btn-sm showBtn btn-fit" data-id="${row.id}">
-                         <i class="fa-regular fa-eye"></i> View
-                        </button>
-                        <a href="/sales/invoice/${row.id}" type="button" class="btn btn-primary btn-sm print btn-fit">
-                         <i class="fa-solid fa-print"></i> Print
-                        </a>
-                        <button type="button" class="btn btn-danger btn-sm deleteBtn btn-fit" data-id="${row.id}">
-                            <i class="fa-solid fa-trash"></i> Delete
-                        </button>
-                    </div>`;
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-success btn-sm showBtn btn-fit" data-id="${row.id}" data-bs-toggle="modal" data-bs-target="#showSalesDetails">
+                                     <i class="fa-regular fa-eye"></i> View
+                                    </button>
+                                    <a href="/sales/invoice/${row.id}" type="button" class="btn btn-primary btn-sm print btn-fit">
+                                     <i class="fa-solid fa-print"></i> Print
+                                    </a>
+                                    <button type="button" class="btn btn-danger btn-sm deleteBtn btn-fit" data-id="${row.id}">
+                                        <i class="fa-solid fa-trash"></i> Delete
+                                    </button>
+                                </div>`;
                             return btns;
                         },
                         targets: 0,
@@ -144,7 +196,53 @@
                 ]
             });
             $("#exportToExcel").click(() => $(".exportToExcel").click());
-        });
 
+            $(document).on("click", ".showBtn", function() {
+                const id = $(this).data("id");
+
+                axios.get(`/sales/fetch/${id}`).then((response) => {
+                    $(".salesEditLink").attr("href", `/sales/${id}/edit`);
+                    const {
+                        sales,
+                        salesDetails
+                    } = response.data.data;
+
+                    $("#salesCode").text(sales.code);
+                    $("#invoiceDate").text(sales.invoice_date);
+                    $("#totalAmount").text("৳ " + sales.total_amount);
+                    $("#paidAmount").text("৳ " + sales.paid_amount);
+                    $("#dueAmount").text("৳ " + sales.due_amount);
+                    $("#salesNote").text(sales.note);
+
+                    // salesDetailsBody
+                    $('#salesDetailsBody').empty();
+                    var finalTotal = 0;
+
+                    // Insert rows into the table body
+                    $.each(salesDetails, function(index, item) {
+                        console.log(item)
+                        const row = `
+                            <tr>
+                                <td>${index + 1}</td>
+                                <td>
+                                    ${item.product_attribute.code}<br>
+                                    ${item.product.name}
+                                    ${item.product_attribute.model != null ? '|' + item.product_attribute.model : ''}
+                                    ${item.product_attribute.color != null ? '|' + item.product_attribute.color : ''}<br>
+                                </td>
+                                <td>${item.qty}</td>
+                                <td>${item.sales_rate} tk</td>
+                                <td>${item.discount} tk</td>
+                                <td>${item.total} tk</td>
+                            </tr>
+                        `;
+                        $('#salesDetailsBody').append(row);
+                        finalTotal += item.total;
+                    });
+                    $("#finalTotal").text(finalTotal);
+                });
+            });
+
+        });
     </script>
 @endpush
