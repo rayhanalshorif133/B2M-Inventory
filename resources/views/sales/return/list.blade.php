d2b610733476fc1fd726ce32fb15090e091a9ee1
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-12-11 15:27:16 +0600

    new sales

--- resources/views/sales/return/list.blade.php
+++ resources/views/sales/return/list.blade.php
@@ -24,7 +24,6 @@
                 </div>
             </div>
         </div>
-        {{-- <sales-return-list-component></sales-return-list-component> --}}
 
         <section class="content">
             <div class="container-fluid">
